c593ba69a3ce83d7b53f590df0e479e90cc3bb06
SChan-Kim <instufld@gmail.com>
2021-02-01 14:18:16 +0900

    add calender

---
+++ cakeMaker/CalenderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalenderViewController.swift
+//  cakeMaker
+//
+//  Created by 김승찬 on 2021/02/01.
+//
+
+import UIKit
+import FSCalendar
+
+class CalenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBOutlet weak var stepLabel: UILabel!
+    @IBOutlet weak var calenderVC: FSCalendar!
+    
+    func updateUI() {
+        stepLabel.text = "Step1. 픽업 날짜를 선택해주세요."
+        
+        // 달력의 년월 글자 바꾸기
+        calenderVC.appearance.headerDateFormat = "YYYY년 M월"
+        
+        // 달력의 요일 글자 바꾸기
+        calenderVC.locale = Locale(identifier: "ko_KR")
+        
+    }
+    
+    
+}
